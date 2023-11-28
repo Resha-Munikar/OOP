5f39f418f61b96e7413787910ad430746b2701e8
Resha-Munikar <resa.munikar@gmail.com>
2023-11-28 19:00:09 +0545

    class to class type conversion added

---
+++ Data Conversion/Class to Class type/1.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+class Inventory1{
+	private:
+		int ino,qty;
+		float rate;
+	public:
+		Inventory1(int a,int b,float c)
+		{
+			ino=a;
+			qty=b;
+			rate=c;
+		}
+		void display()
+		{
+			cout<<endl<<"Item number: "<<ino;
+			cout<<endl<<"Quantity: "<<qty;
+			cout<<endl<<"Rate: "<<rate;
+		}
+		int getinfo()
+		{
+			return ino;
+		}
+		float getamt()
+		{
+			return(qty*rate);
+		}
+};
+class Inventory2{
+	private:
+		int ino;
+		float amount;
+	public:
+		void operator = (Inventory1 I)
+		{
+			ino=I.getinfo();
+			amount=I.getamt();
+		}
+		void display()
+		{
+			cout<<endl<<"Item number: "<<ino;
+			cout<<endl<<"Amount: "<<amount;
+		}
+};
+int main()
+{
+	Inventory1 i1(5,10,25);
+	Inventory2 i2;
+	i2=i1;
+	i1.display();
+	i2.display();
+	return 0;
+}
